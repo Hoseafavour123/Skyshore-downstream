fc1dffd4b6f49b4d493e6fb64b2ae16045bd82c5
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2025-01-05 15:15:11 +0100

    Hot fix

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -12,7 +12,7 @@ const Services: React.FC = () => {
 
         <div className="mt-5 bg-gradient-to-r from-purple-200 via-blue-200 to-white p-12 rounded-xl shadow-lg">
           <div className="max-w-6xl mx-auto text-white">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:p-6">
               {services.map((service, id) => (
                 <ServiceCard
                   key={id}
